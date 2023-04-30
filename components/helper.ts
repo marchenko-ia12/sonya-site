4683f25212ddda84ded0ef90e7216417476964a6
HeeHO <marchenkoia12@gmail.com>
2023-04-30 19:49:29 +0100

    -update pictures

--- components/helper.ts
+++ components/helper.ts
@@ -2,6 +2,6 @@ const importAll = (r: any) => {
 	return r.keys().map(r);
 }
 
-const images = importAll(require.context('../arts/', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('../arts/', false, /\.(png|jpe?g|webp)$/));
 
 export default images;
